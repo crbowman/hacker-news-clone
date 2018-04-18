dd79c5133ad6d12235aad51fa0c52bfa30523892
crbowman <c.bowman1711@gmail.com>
2018-04-18 19:01:39 -0400

    get barebones server working

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [com.datomic/datomic-free "0.9.5697"]
                  [http-kit "2.2.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-core]]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-json "0.4.0"]]
   :dev-dependencies [[ring/ring-devel "1.6.3"]]
   :plugins [[lein-ring "0.12.4"]]
-  :ring {:handler hacker-news-clone.core/app}
-  :main hacker-news-clone.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :ring {:handler hacker-news-clone.core/app})
--- src/hacker_news_clone/core.clj
+++ src/hacker_news_clone/core.clj
@@ -1,7 +1,45 @@
 (ns hacker-news-clone.core
-  (:gen-class))
+  (:use [compojure.core :as c]
+        [ring.util.response]
+        [ring.middleware.content-type]
+        [ring.middleware.params :refer [wrap-params]]
+        [ring.middleware.json :refer [wrap-json-body wrap-json-response]])
+  (:require [clojure.tools.logging :refer [info error]]
+            [clojure.string :as string]
+            [compojure.route :as route]
+            [compojure.handler :as handler])
+  (:gen-class :main true))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn wrap-fail-safe [handler]
+  "Wraps a safe failure response"
+  (fn [req]
+    (try
+      (handler req)
+      (catch Exception e
+        (error e)
+        {:status 422
+         :body {:errors {:body ["Server error due to wrong user input."]}}}))))
+
+(defn wrap-access-log [handler]
+  "Wraps the server log for each request received."
+  (fn [req]
+    (info "==> " (req :uri))
+    (let [response (handler req)]
+      (info "<== " (req :uri) "[" (response :status) "]")
+      response)))
+
+(defn wrap-middlewares [routes]
+  "Wraps routes with all common middlewares."
+  (-> routes
+      wrap-fail-safe
+      wrap-params
+      wrap-json-response
+      wrap-access-log
+      (wrap-json-body {:kewords? true})))
+
+(c/defroutes public-routes
+  (c/GET "/resource-status" [] "OK")
+  (c/GET "/health" [] "Service healthy!"))
+
+(def app
+  (wrap-middlewares public-routes))
