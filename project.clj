a7a1a8b13b9d70e89f99b277e141b1c51dfe1bc3
crbowman <c.bowman1711@gmail.com>
2018-04-18 16:32:01 -0400

    first update to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,15 @@
 (defproject hacker-news-clone "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Hacker News clone in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main ^:skip-aot hacker-news-clone.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-core]]
+  :dev-dependencies [[ring/ring-devel "1.6.3"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler hacker-news-clone.core/app}
+  :main hacker-news-clone.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
